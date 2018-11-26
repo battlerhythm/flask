36f94188dd1ddeb5481e03c3809e8480135273a4
Jeongmin Oh <battlerhythm@gmail.com>
2018-11-25 23:13:03 -0500

    update

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -30,3 +30,39 @@ def create_app(test_config=None):
         return 'Hello, World!'
 
     return app
+
+# __init__.py serves double duty:
+# it will contain the application factory, and
+# it tells Python that the flaskr directory should be treated as a package.
+
+
+# create_app is the application factory function. You’ll add to it later in the tutorial, but it already does a lot.
+#
+# 1. app = Flask(__name__, instance_relative_config=True) creates the Flask instance.
+#     * __name__ is the name of the current Python module. The app needs to know where it’s located to set up some paths, and __name__ is a convenient way to tell it that.
+#     * instance_relative_config=True tells the app that configuration files are relative to the instance folder. The instance folder is located outside the flaskr package and can hold local data that shouldn’t be committed to version control, such as configuration secrets and the database file.
+# 2. app.config.from_mapping() sets some default configuration that the app will use:
+#     * SECRET_KEY is used by Flask and extensions to keep data safe. It’s set to 'dev' to provide a convenient value during development, but it should be overridden with a random value when deploying.
+#     * DATABASE is the path where the SQLite database file will be saved. It’s under app.instance_path, which is the path that Flask has chosen for the instance folder. You’ll learn more about the database in the next section.
+# 3. app.config.from_pyfile() overrides the default configuration with values taken from the config.py file in the instance folder if it exists. For example, when deploying, this can be used to set a real SECRET_KEY.
+#     * test_config can also be passed to the factory, and will be used instead of the instance configuration. This is so the tests you’ll write later in the tutorial can be configured independently of any development values you have configured.
+# 4. os.makedirs() ensures that app.instance_path exists. Flask doesn’t create the instance folder automatically, but it needs to be created because your project will create the SQLite database file there.
+# 5. @app.route() creates a simple route so you can see the application working before getting into the rest of the tutorial. It creates a connection between the URL /hello and a function that returns a response, the string 'Hello, World!' in this case.
+
+
+# Run the application
+
+# For Linux and Mac:
+# export FLASK_APP=flaskr
+# export FLASK_ENV=development
+# flask run
+
+# For Windows cmd, use set instead of export:
+# set FLASK_APP=flaskr
+# set FLASK_ENV=development
+# flask run
+
+# For Windows PowerShell, use $env: instead of export:
+# $env:FLASK_APP = "flaskr"
+# $env:FLASK_ENV = "development"
+# flask run
